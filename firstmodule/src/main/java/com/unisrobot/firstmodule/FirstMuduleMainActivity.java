637178deed31992976441031b4c3aa2a5f293b9c
yinxiaowei <373736762@qq.com>
2018-04-13 18:33:55 +0800

    模块化的实现。。。已经ok

--- firstmodule/src/main/java/com/unisrobot/firstmodule/FirstMuduleMainActivity.java
+++ firstmodule/src/main/java/com/unisrobot/firstmodule/FirstMuduleMainActivity.java
@@ -4,14 +4,23 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.baronzhang.android.router.RouterInjector;
 import com.baronzhang.android.router.annotation.inject.Inject;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by Administrator on 2018/4/13.
  */
 
+/**
+ * library中要使用最新的butterknife，，所有的id用R2 来代替。
+ */
 public class FirstMuduleMainActivity extends Activity {
 
         private static final String TAG = "FirstMuduleMainActivity";
@@ -22,10 +31,19 @@ public class FirstMuduleMainActivity extends Activity {
         @Inject("brokerIdList")
         String lists;
 
+        @BindView(R2.id.first_module_button)
+        Button firstModuleButton;
+
+        @OnClick(R2.id.first_module_button)
+        public void testBtn() {
+                Toast.makeText(this, "show butterknife", Toast.LENGTH_SHORT).show();
+        }
+
         @Override
         protected void onCreate(@Nullable Bundle savedInstanceState) {
                 super.onCreate(savedInstanceState);
                 setContentView(R.layout.first_module_activity_main);
+                ButterKnife.bind(this);
 
                 // 传递的参数通过注解获取
                 /**
