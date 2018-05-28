36e616080ded5d26832a7cf3834afbd77287ca6c
yinxiaowei <373736762@qq.com>
2018-05-28 12:37:05 +0800

    add

--- BaseFrame/src/main/java/com/uurobot/baseframe/activitys/BaseActivity.java
+++ BaseFrame/src/main/java/com/uurobot/baseframe/activitys/BaseActivity.java
@@ -23,6 +23,12 @@ public class BaseActivity extends FragmentActivity {
                         WindowManager.LayoutParams.FLAG_FULLSCREEN);
                 getWindow().setFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON,
                         WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        }
+
+        @Override
+        protected void onResume() {
+                super.onResume();
                 hideBottomUIMenu();
         }
 
