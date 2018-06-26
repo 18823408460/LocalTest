ed9b2f6c5589829e54b650f31034f3dc289a9c91
373736762 <373736762@qq.com>
2018-06-26 23:12:18 +0800

    add

---
+++ robothead/src/main/java/com/unisrobot/robothead/bluetooth/Node.java
@@ -0,0 +1,11 @@
+package com.unisrobot.robothead.bluetooth;
+
+/**
+ * Created by WEI on 2018/6/26.
+ */
+
+public abstract class Node {
+    abstract void start();
+
+    abstract void end();
+}
--- robothead/src/main/java/com/unisrobot/robothead/visualedit/VisualEditActivity.java
+++ robothead/src/main/java/com/unisrobot/robothead/visualedit/VisualEditActivity.java
@@ -10,7 +10,12 @@ import com.unisrobot.robothead.bluetooth.IBluetoothLisenter;
 /**
  * Created by Administrator on 2018/6/26.
  * <p>
- * 可视化编程测试
+ * 可视化编程测试, 每个节点应该怎么执行?
+ * 1. 普通节点,直接执行（需要根据不同的类型，换算对应的时间）
+ * 2. 带有逻辑关系的节点，需要找到节点 执行的条件，节点执行完毕的条件,
+ *    条件: 执行次数, 传感器, 逻辑运算,
+ *
+ * 3.节点什么结束完毕？？
  */
 
 public class VisualEditActivity extends Activity {
@@ -36,5 +41,11 @@ public class VisualEditActivity extends Activity {
 
     private void parseBlueDataThread(String data) {
 
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        bluToothMgr.destroy();
     }
 }
